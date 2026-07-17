27e06ccf56b7ddb62b336739535440cb9542c9b8
balachandar8 <balachandar1998try@gmail.com>
2026-07-17 23:43:25 +0530

    second day

---
+++ SentenceReveString.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SentenceReveString{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the String Sentence: ");
+
+        String input = scanner.nextLine();
+
+        String[] words = input.split(" ");
+
+       String reverse = "" ; 
+
+        char ch ; 
+
+        
+
+        for (int i = 0; i < words.length; i++){
+
+            String word = words [i];
+            String nstr = " ";
+
+             
+
+            for (int j=0; j< word.length(); j++){
+
+                ch = word.charAt(j);
+
+                nstr = ch + nstr; 
+
+            }
+
+            reverse =  reverse + nstr ;      
+            
+        }
+
+        System.out.println("Reverse words string : "+ reverse);       
+
+
+    }
+}
