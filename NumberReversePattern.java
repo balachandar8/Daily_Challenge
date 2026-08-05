3a581cced486b01bb4d0d05346495b838f77f917
balachandar8 <balachandar1998try@gmail.com>
2026-08-05 23:03:19 +0530

    0508 single progarm

---
+++ NumberReversePattern.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NumberReversePattern {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Enter the number :");
+
+        int input = scanner.nextInt();
+
+        for(int i= input; i>= 1; i--){
+
+            for(int j=1; j<=i; j++){
+
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+
+
+
+       scanner.close();
+
+    }
+    
+}
